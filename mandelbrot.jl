da843672cc762e626d7c3f2675c8017c8edb951d
Nicolas Smith <nicolas@gooeyfaze.com>
2021-12-11 15:39:51 -0800

    Reformatting.

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -16,7 +16,7 @@ function recurse_steps_to_satisfy(
 end
 
 function mandelbrot_function(c::Complex)
-    return (z) -> z*z + c
+    return (z) -> z * z + c
 end
 
 function mandelbrot_condition(z::Complex)
@@ -29,8 +29,31 @@ function mandelbrot_steps(c::Complex, max_steps::Integer)
 end
 
 function get_cmap(colorscheme::ColorScheme, max_steps::Integer)
-    return [colorscheme[num_steps / (max_steps + 1)] 
-        for num_steps in 1:max_steps+1]
+    return [colorscheme[num_steps/(max_steps+1)]
+            for num_steps = 1:max_steps+1]
+end
+
+function get_complex_domain(pixel_size, center_loc, width, height)
+    horizontal_extent = width * pixel_size
+    vertical_extent = height * pixel_size
+    real_domain = (
+        center_loc[1]-horizontal_extent/2:
+        pixel_size/resample_factor:
+        center_loc[1]+horizontal_extent/2
+        )
+    imaginary_domain = (
+        center_loc[2]-vertical_extent/2:
+        pixel_size/resample_factor:
+        center_loc[2]+vertical_extent/2
+        )
+    
+    # The imaginary domain is reversed so that the image
+    # comes out with the correct orientation.
+    imaginary_domain = reverse(imaginary_domain)
+
+    complex_domain = [Complex(x, y) for y in imaginary_domain, x in real_domain]
+
+    return complex_domain
 end
 
 # Color space.
@@ -41,27 +64,20 @@ max_steps = 2500
 pixel_size = 0.0000001
 
 resample_factor = 2
-blur_kernel = Kernel.gaussian(resample_factor/2)
+blur_kernel = Kernel.gaussian(resample_factor / 2)
 
 width = 1920
 height = 1080
 
 center_loc = (-0.75393, 0.05)
 
-horizontal_extent = width * pixel_size
-vertical_extent = height * pixel_size
-real_domain = center_loc[1]-horizontal_extent/2:pixel_size/resample_factor:center_loc[1]+horizontal_extent/2
-imaginary_domain = center_loc[2]-vertical_extent/2:pixel_size/resample_factor:center_loc[2]+vertical_extent/2
-# The imaginary domain is reversed so that the image comes out with the correct orientation.
-imaginary_domain = reverse(imaginary_domain)
-
-complex_domain = [Complex(x, y) for y in imaginary_domain, x in real_domain]
+complex_domain = get_complex_domain(pixel_size, center_loc, width, height)
 color_map = get_cmap(colorscheme, max_steps)
 
 @time begin
-    image = @pipe complex_domain |> 
+    image = @pipe complex_domain |>
         CuArray |> # Casting as CuArray sends the computation to the GPU.
-        mandelbrot_steps.(_, max_steps) |> 
+        mandelbrot_steps.(_, max_steps) |>
         Array |>
         color_map[_] |>
         imfilter(_, blur_kernel) |>
